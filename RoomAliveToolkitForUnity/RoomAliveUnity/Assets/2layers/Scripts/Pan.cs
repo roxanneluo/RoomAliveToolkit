af32eb686356d0481b76b9b00dbc45de99ffa830
Xuan Luo <xuanluo@cs.washington.edu>
2017-12-18 02:15:29 -0800

    added script Pan to Pan camera over the scene

---
+++ RoomAliveToolkitForUnity/RoomAliveUnity/Assets/2layers/Scripts/Pan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan : MonoBehaviour {
+	public Vector2 panRadius;
+	public Vector2 v;
+	public Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		transform.position = new Vector3 (startPos.x - panRadius.x,
+			startPos.y - panRadius.y, startPos.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y > startPos.y + panRadius.y) {
+			transform.position = new Vector3 (startPos.x - panRadius.x,
+				startPos.y - panRadius.y, startPos.z);
+			return;
+		}
+
+		if (transform.position.x > startPos.x + panRadius.x ||
+			transform.position.x < startPos.x - panRadius.x) {
+			transform.position += new Vector3 (0, v.y, 0);
+			v.x = -v.x;
+		}
+
+		Vector3 vx = new Vector3 (v.x, 0, 0);
+		transform.position += vx;
+	}
+}
