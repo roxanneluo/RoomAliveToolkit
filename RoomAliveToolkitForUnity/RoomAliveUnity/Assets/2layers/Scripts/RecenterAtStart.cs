7453846931f49eb964826b4a0cdedefda50b2a13
Xuan Luo <xuanluo@cs.washington.edu>
2017-12-28 20:18:44 -0800

    added recenter at start

---
+++ RoomAliveToolkitForUnity/RoomAliveUnity/Assets/2layers/Scripts/RecenterAtStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecenterAtStart : MonoBehaviour {
+	public GameObject obj;
+	// Use this for initialization
+	void Start () {
+		Vector3 start_pos = transform.position;
+		obj.transform.position = start_pos;
+	}
+}
